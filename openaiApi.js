433d4c07ab808f235b9261fdef69430f95c7c5ae
Lucycan LY <95648904+looSeeCan@users.noreply.github.com>
2023-03-30 19:53:49 -0500

    issues
    oK fixed it by messing with the temperature and max tokens settings

--- openaiApi.js
+++ openaiApi.js
@@ -12,6 +12,8 @@ async function runCompletion(question) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: question,
+    temperature: 0.5,
+    max_tokens: 100,
   });
   console.log(completion.data.choices[0].text);
   return completion.data.choices[0].text;
