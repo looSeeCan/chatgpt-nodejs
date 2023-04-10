3ee2314b2c477bd6e076ad4d62c29eb3dc9a99c0
looSeeCan2 <110320951+looSeeCan2@users.noreply.github.com>
2023-04-10 18:12:14 -0500

    -m

--- utils/openaiApi.js
+++ utils/openaiApi.js
@@ -12,8 +12,8 @@ async function runCompletion(question) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: question,
-    temperature: 0.5,
-    max_tokens: 100,
+    temperature: 0.1,
+    max_tokens: 200,
   });
   console.log(completion.data.choices[0].text);
   return completion.data.choices[0].text;
