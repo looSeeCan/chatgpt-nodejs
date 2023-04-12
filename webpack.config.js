f23ed31479ad135e084817a7ccc6617c7d43b2be
Lucycan LY <95648904+looSeeCan@users.noreply.github.com>
2023-04-12 18:34:10 -0500

    build process
    worked on a build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
